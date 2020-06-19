775d4816052fb539c6d63eeb2dc7032e941d2d82
Hilarius Bayu <hilariuzbayu@outlook.com>
2020-06-19 14:00:01 +0700

    Mencoba mengenal props pada React.js

--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Blog from './Blog'
 
 const Home = () => {
-    return <div><h1>Ini Adalah Home</h1></div>
+    return <div>
+        <Blog
+            tanggal="02 Juni 2020"
+            judul="Teknologi Blockchain"
+            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        />
+         <Blog
+            tanggal="03 Juni 2020"
+            judul="Teknologi Internet of Things"
+            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        />
+         <Blog
+            tanggal="04 Juni 2020"
+            judul="Design Pattern Laravel"
+            summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        />
+
+    </div>
 }
 
 export default Home;
