00820b011e74b5764d5313dc8ea2393a0ef11c35
Hilarius Bayu <hilariuzbayu@outlook.com>
2020-07-06 00:02:02 +0700

    Try State juga Props

--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import Blog from './Blog'
+// import Blog from './Blog'
+import Produk from '../Class/Produk';
 
 const Home = () => {
     return <div>
-        <Blog
+        { 
+        <div>
+        <Produk nama="ROG Zephyrus G14" stock="10" harga="14.900.000"/>
+        <Produk nama="ROG Zephyrus GXS" stock="5" harga="17.900.000"/>
+        <Produk nama="ROG Zephyrus G71" stock="20" harga="11.900.000"/>
+        <Produk nama="ROG Zephyrus G18" stock="30" harga="12.900.000"/>
+        <Produk nama="ROG Zephyrus G69" stock="40" harga="19.900.000"/>
+        <Produk nama="ROG Zephyrus G361" stock="35" harga="12.900.000"/>
+        </div>
+        /* <Blog
             tanggal="02 Juni 2020"
             judul="Teknologi Blockchain"
             summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
@@ -17,7 +27,7 @@ const Home = () => {
             tanggal="04 Juni 2020"
             judul="Design Pattern Laravel"
             summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-        />
+        /> */}
 
     </div>
 }
