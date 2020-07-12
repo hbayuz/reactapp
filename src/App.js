bf791c0394842e50c8d47237c18cd11e182cf262
Hilarius Bayu <hilariuzbayu@outlook.com>
2020-07-12 21:51:25 +0700

    mencoba Passing data dengan react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavbarComp from './Component/Fungsional/NavbarComp';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './Component/Fungsional/HomePage';
 import About from './Component/Fungsional/AboutComp';
+import DetailComp from './Component/Fungsional/DetailComp';
 
 // import BootstrapComp from './Component/Class/BootstrapComp';
 // import Parent from './Component/Class/Parent';
@@ -18,6 +19,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/detail/:id" component={DetailComp} />
       </Switch>
     </BrowserRouter>
 
